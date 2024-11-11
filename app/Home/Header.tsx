909ed34a664d0f0655ade82d648fc78d57abd802
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-11-11 17:01:55 +0530

    Layout

--- app/Home/Header.tsx
+++ app/Home/Header.tsx
@@ -1,9 +1,14 @@
+"use client";
 import Themes from "@/components/Themes";
 import Image from "next/image";
 
-import React from "react";
+import React, { useState } from "react";
+import { SITE_DATA } from "../data/site-data";
+import { MenuButton } from "@/components/framer/MenuButton";
 
 function Header() {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <header className="w-full h-[100px] flex justify-between items-center fixed bg-white dark:bg-[#040718] px-4">
       <div className="relative h-full aspect-[2/1]">
@@ -16,7 +21,32 @@ function Header() {
         />
       </div>
 
-      <div className="flex items-center">
+      <div>
+        <div className="hidden lg:flex justify-between w-[500px]">
+          {SITE_DATA.menu.map((menu, idx) => {
+            return (
+              <a href={menu.url} key={idx}>
+                {menu.title}
+              </a>
+            );
+          })}
+        </div>
+
+        <div className="lg:hidden flex">
+          <MenuButton
+            isOpen={isOpen}
+            onClick={() => setOpen(!isOpen)}
+            strokeWidth="8"
+            color="#FFF"
+            lineProps={{ strokeLinecap: "round" }}
+            transition={{ type: "spring", stiffness: 260, damping: 20 }}
+            width="34"
+            height="24"
+          />
+        </div>
+      </div>
+
+      <div className="hidden lg:flex items-center">
         <Themes />
       </div>
     </header>
---
+++ app/data/site-data.tsx
@@ -0,0 +1,24 @@
+export const SITE_DATA = {
+  menu: [
+    {
+      title: "Home",
+      url: "/",
+    },
+    {
+      title: "About",
+      url: "about",
+    },
+    {
+      title: "Contact",
+      url: "contact",
+    },
+    {
+      title: "News",
+      url: "news",
+    },
+    {
+      title: "Blog",
+      url: "blog",
+    },
+  ],
+};
