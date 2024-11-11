6200efb6f910d5b1b24dc10371822bc872ac44f2
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-11-11 15:46:18 +0530

    Header

--- app/Home/Header.tsx
+++ app/Home/Header.tsx
@@ -1,11 +1,24 @@
+import Themes from "@/components/Themes";
 import Image from "next/image";
 
 import React from "react";
 
 function Header() {
   return (
-    <header className=" ">
-      <Image src={"/logo.svg"} alt={""} width={200} height={200} />
+    <header className="w-full h-[100px] flex justify-between items-center fixed bg-white px-4">
+      <div className="relative h-full aspect-[2/1]">
+        <Image
+          src={"/logo.svg"}
+          alt={""}
+          width={400}
+          height={400}
+          className="-mt-8"
+        />
+      </div>
+
+      <div className="flex items-center">
+        <Themes />
+      </div>
     </header>
   );
 }
