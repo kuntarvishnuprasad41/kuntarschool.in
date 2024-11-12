649a94d7b22e1e87b792b51a2717f1005d59172a
Vishnu Prasad Kuntar <kuntarvishnuprasad41@gmail.com>
2024-11-12 12:18:42 +0530

    Homr

--- components/shared/Hero.tsx
+++ components/shared/Hero.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Hero() {
   return (
     <div>
-      <div className="relative    z-1  h-[523px] w-full max-w-[1100px]    ">
+      <div className="relative    z-1  h-[523px] w-full max-w-[1100px]  p-2  ">
         Something Cool is coming soon, Stay tuned!
       </div>
     </div>
